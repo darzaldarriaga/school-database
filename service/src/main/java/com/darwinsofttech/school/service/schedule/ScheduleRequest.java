1e08c115f3bcf3c3b8d04a846b8871d337e2e669
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-02 11:54:53 +0800

    added request form for schedules

---
+++ service/src/main/java/com/darwinsofttech/school/service/schedule/ScheduleRequest.java
@@ -0,0 +1,40 @@
+package com.darwinsofttech.school.service.schedule;
+
+public class ScheduleRequest {
+    private int id;
+    private int subjectId;
+    private int teacherId;
+    private int studentId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
