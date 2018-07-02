a6c0d44d43f1a586dcb8675ba3880a1a42165ffe
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-02 11:52:09 +0800

    changed return type of findByIdWithStudents to Optional

--- repository/src/main/java/com/darwinsofttech/school/repository/schedule/ScheduleRepository.java
+++ repository/src/main/java/com/darwinsofttech/school/repository/schedule/ScheduleRepository.java
@@ -3,8 +3,10 @@ package com.darwinsofttech.school.repository.schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
 
     @Query("SELECT s FROM Schedule s JOIN FETCH s.students WHERE s.id = ?1")
-    Schedule findScheduleById(int id);
+    Optional<Schedule> findByIdWithStudents(int id);
 }
