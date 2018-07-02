5ba8717c75bc1e0a7ba394c2d00d7721de612581
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-02 11:48:08 +0800

    added basic functions

--- rest-controller/src/main/java/com/darwinsofttech/restcontroller/student/TeacherRestController.java
+++ rest-controller/src/main/java/com/darwinsofttech/restcontroller/student/TeacherRestController.java
@@ -1,11 +1,11 @@
 package com.darwinsofttech.restcontroller.student;
 
+import com.darwinsofttech.school.repository.teacher.Teacher;
+import com.darwinsofttech.school.service.teacher.TeacherRequest;
 import com.darwinsofttech.school.service.teacher.TeacherResponse;
 import com.darwinsofttech.school.service.teacher.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,32 @@ public class TeacherRestController {
     @GetMapping("/list")
     public List<TeacherResponse> findAll() {
         return teacherService.findAll();
+    }
+
+    @GetMapping("/findOne")
+    public TeacherResponse findById(@RequestParam(name = "id") int id) {
+        return teacherService.findById(id);
+    }
+
+    @PostMapping("/create")
+    public TeacherResponse create(@RequestBody TeacherRequest teacherRequest) {
+        teacherService.save(teacherRequest.getLastName(), teacherRequest.getFirstName(), teacherRequest.getMiddleName());
+        return new TeacherResponse();
+    }
+
+    @PostMapping("/update")
+    public TeacherResponse update(@RequestBody TeacherRequest teacherRequest) {
+        teacherService.update(teacherRequest);
+        return new TeacherResponse(
+                teacherRequest.getId(),
+                teacherRequest.getLastName(),
+                teacherRequest.getFirstName(),
+                teacherRequest.getMiddleName()
+        );
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam(name = "id") int id) {
+        teacherService.remove(id);
     }
 }
