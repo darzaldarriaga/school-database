1b75537fb9f68ea113b703545a8c990903994c1d
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-02 11:48:59 +0800

    created request object for subjects

---
+++ service/src/main/java/com/darwinsofttech/school/service/subject/SubjectRequest.java
@@ -0,0 +1,31 @@
+package com.darwinsofttech.school.service.subject;
+
+public class SubjectRequest {
+    private int id;
+    private String subjectCode;
+    private String subjectDescription;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getSubjectDescription() {
+        return subjectDescription;
+    }
+
+    public void setSubjectDescription(String subjectDescription) {
+        this.subjectDescription = subjectDescription;
+    }
+}
