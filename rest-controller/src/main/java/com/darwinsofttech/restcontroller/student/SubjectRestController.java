237c8a3e5f0a6221ccc5993564651965cd876f25
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-02 11:50:38 +0800

    added basic functions to subject rest controller

--- rest-controller/src/main/java/com/darwinsofttech/restcontroller/student/SubjectRestController.java
+++ rest-controller/src/main/java/com/darwinsofttech/restcontroller/student/SubjectRestController.java
@@ -1,12 +1,11 @@
 package com.darwinsofttech.restcontroller.student;
 
 import com.darwinsofttech.school.repository.subject.Subject;
+import com.darwinsofttech.school.service.subject.SubjectRequest;
 import com.darwinsofttech.school.service.subject.SubjectResponse;
 import com.darwinsofttech.school.service.subject.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,28 @@ public class SubjectRestController {
     @GetMapping("/list")
     public List<SubjectResponse> findAll() {
         return subjectService.findAll();
+    }
+
+    @GetMapping("/findOne")
+    public SubjectResponse findById(@RequestParam(name = "id") int id) {
+        return subjectService.findById(id);
+    }
+
+    @PostMapping("/create")
+    public SubjectResponse create(@RequestBody SubjectRequest subjectRequest) {
+        subjectService.save(subjectRequest.getSubjectCode(), subjectRequest.getSubjectDescription());
+        return new SubjectResponse();
+    }
+
+    @PostMapping("/update")
+    public SubjectResponse update(@RequestBody SubjectRequest subjectRequest) {
+        subjectService.update(subjectRequest);
+        return new SubjectResponse(subjectRequest.getId(), subjectRequest.getSubjectCode(),
+                subjectRequest.getSubjectDescription());
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam(name = "id") int id) {
+        subjectService.remove(id);
     }
 }
