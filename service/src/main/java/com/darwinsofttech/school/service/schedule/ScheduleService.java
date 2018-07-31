a7cd6ffe31fe8c70903ac0102cb8da1de2839118
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-31 12:02:51 +0800

    added removeStudent function

--- service/src/main/java/com/darwinsofttech/school/service/schedule/ScheduleService.java
+++ service/src/main/java/com/darwinsofttech/school/service/schedule/ScheduleService.java
@@ -1,13 +1,12 @@
 package com.darwinsofttech.school.service.schedule;
 
-import com.darwinsofttech.school.repository.schedule.Schedule;
-
 import java.util.List;
 
 public interface ScheduleService {
     void create(ScheduleRequest scheduleRequest);
     void update(ScheduleRequest scheduleRequest);
     void addStudent(ScheduleRequest scheduleRequest);
+    void removeStudent(ScheduleRequest scheduleRequest);
     void delete(int scheduleId);
     List<ScheduleResponse> findAll();
     ScheduleResponse findById(int scheduleId);
