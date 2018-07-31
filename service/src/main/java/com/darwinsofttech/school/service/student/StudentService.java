0ecf0ae7b36937f6b7c8da234245ad0535585a58
Darwin D. Zaldarriaga <darzaldarriaga@gmail.com>
2018-07-31 12:06:39 +0800

    changed return type of save method

--- service/src/main/java/com/darwinsofttech/school/service/student/StudentService.java
+++ service/src/main/java/com/darwinsofttech/school/service/student/StudentService.java
@@ -5,7 +5,7 @@ import com.darwinsofttech.school.repository.student.Student;
 import java.util.List;
 
 public interface StudentService {
-    void save(String lastName, String firstName, String middleName);
+    Student save(String lastName, String firstName, String middleName);
     void update(StudentRequest studentRequest);
     void remove(int studentId);
     List<StudentResponse> findAll();
